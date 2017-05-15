82fc85aacff1b29f8ba78f9b3188506c774db325
Benjamin <201410819@post.au.dk>
2017-05-15 14:38:10 +0200

    udkast til fileClient

--- Exercise_12_cpp/Exercise_12_cpp_Client/file_client.cpp
+++ Exercise_12_cpp/Exercise_12_cpp_Client/file_client.cpp
@@ -1,12 +1,14 @@
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <iostream>
 #include <cstring>
-#include <cstdlib>
 #include "../include/Transport.h"
 #include "../include/lib.h"
 #include "file_client.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -29,7 +31,15 @@ using namespace std;
 /// </param>
 file_client::file_client(int argc, char **argv)
 {
-    // TO DO Your own code
+    Transport::Transport * transport = new Transport::Transport(BUFSIZE);
+
+    //Sende filnavn
+    transport->send(argv[1],sizeof(argv[1]));
+
+
+    //Modtage fil
+    string fileName(argv[1]);
+    receiveFile(fileName,transport);
 }
 
 /// <summary>
@@ -43,7 +53,36 @@ file_client::file_client(int argc, char **argv)
 /// </param>
 void file_client::receiveFile (std::string fileName, Transport::Transport *transport)
 {
-        // TO DO Your own code
+    int fd;
+    char receive_buf[BUFSIZE];
+    long fileSize;
+
+    transport->receive(receive_buf,BUFSIZE);
+    fileSize = atol(receive_buf);
+
+    //Åbner fil
+
+     fd = open(fileName.c_str(), O_WRONLY | O_CREAT,S_IXGRP);
+
+
+     //Modtager fil
+     for (int i = 0; i<fileSize/BUFSIZE+1; i++)
+     {
+         bzero(receive_buf,BUFSIZE); //Tømmer bufferen
+
+         if (i<fileSize/BUFSIZE) //Så længe der er mere end 1000 tilbage
+         {
+             read(fd,receive_buf,BUFSIZE);
+             transport->receive(receive_buf, BUFSIZE);
+         }
+
+         else
+         {
+             read(fd,receive_buf,(fileSize%BUFSIZE));
+             transport->receive(receive_buf, (fileSize%BUFSIZE));
+
+         }
+     }
 }
 
 /// <summary>
@@ -55,19 +94,21 @@ void file_client::receiveFile (std::string fileName, Transport::Transport *trans
 int main(int argc, char** argv)
 {
 
+    file_client * client = new file_client(argc, argv);
+
+    /*
     Transport::Transport trans(BUFSIZE);
 
    char recv_buf[100];
    trans.receive(recv_buf, 100);
    cout << "Besked: " << recv_buf << endl;
-   /*
+
    while(1)
    {
    link.receive(recv_buf, 100);
 
    cout << recv_buf << endl;
    }
-    //new file_client(argc, argv);
     */
 
     return 0;
